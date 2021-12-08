2590a5249fb097f091b7c59be8621a9a0991e780
Uttam Marandi <uttammarandi007@gmail.com>
2021-12-08 12:49:30 +0530

    touchable component

--- src/components/TextImageView.js
+++ src/components/TextImageView.js
@@ -1,8 +1,6 @@
-// Button component
-
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   // console.log("App executed");
@@ -14,8 +12,16 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>Hello React Native!</Text> 
       <StatusBar style="auto" />
+      {/* Import local image */}
       <Image source = {require("./assets/favicon.png")}/>
-      
+      {/* Import external image , require height and width set */}
+      <Image 
+        blurRadius={1} //blurs the image
+        fadeDuration={1000}//fade in on load
+        source={{
+        width: 200,
+        height: 300,
+        uri:"https://i.picsum.photos/id/960/200/300.jpg?hmac=lMLI27rQoFOVbWUEFQBILnVA_jFYw2b8KHACIGkDLD4"}}/>
     </SafeAreaView>
     //View is like a div in react native
     //SafeAreaView also takes notches/teardrops into consideration. so applies padding automatically
@@ -23,8 +29,6 @@ export default function App() {
     //Text is like the p tag of html
     //numberOfLines depicts how many lines are allowed for this Text component
     //onPress allows to run a function when the Text is clicked
-
-    
   );
 }
 
@@ -36,9 +40,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
 
   },
-  padding:{
-    margin: 10,
-  }
 });
 
 
